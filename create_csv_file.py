279901a0041994ce1fb07e1fc1fc6b90226331fb
redboo <garda1@mail.ru>
2023-05-10 14:44:37 +0500

    возвращает абсолютный путь до файла

--- create_csv_file.py
+++ create_csv_file.py
@@ -1,4 +1,5 @@
 import csv
+import os
 from datetime import datetime
 
 from constants import DOWNLOAD_DIR
@@ -19,18 +20,19 @@ CSV_HEADER = [
 
 def create_csv_file(download_dir: str = DOWNLOAD_DIR, filename_suffix: str = "evmos") -> str:
     """
-    Создает новый CSV-файл в указанном каталоге загрузки с именем, содержащим текущую дату и время, и заданным суффиксом.
-    Записывает заголовки столбцов в файл. Возвращает имя созданного файла.
+    Создает новый CSV-файл в указанном каталоге загрузки с именем, содержащим текущую дату и время,
+    и заданным суффиксом. Записывает заголовки столбцов в файл. Возвращает абсолютный путь до созданного файла.
 
     Аргументы:
-        - `download_dir (str, optional)`: Каталог загрузки для сохранения файла. По умолчанию используется глобальная константа `DOWNLOAD_DIR`.
+        - `download_dir (str, optional)`: Каталог загрузки для сохранения файла.
+        По умолчанию используется глобальная константа `DOWNLOAD_DIR`.
         - `filename_suffix (str, optional)`: Суффикс имени файла. По умолчанию используется значение `"evmos"`.
 
     Возвращает:
-        `str`: Имя созданного файла в формате `"YYYY-MM-DD_HH-MM-SS_suffix.csv"`.
+        `str`: Абсолютный путь до созданного файла в формате `"YYYY-MM-DD_HH-MM-SS_suffix.csv"`.
     """
     filename = f"{download_dir}/{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}_{filename_suffix}.csv"
     with open(filename, "a", encoding="utf-8", newline="") as file:
         writer = csv.writer(file)
         writer.writerow(CSV_HEADER)
-    return filename
+    return os.path.abspath(filename)
